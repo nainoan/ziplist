2d982590e5618aa063c06189cea1e9a29f0d385f
Nainoa Jorg <51459240+nainoan@users.noreply.github.com>
2019-09-23 21:24:25 -1000

    2nd attempt at the zip functions
    zip just makes objects containing the arrays, but flatten can make them all one array. Going to remember this for later.

--- ziplist.js
+++ ziplist.js
@@ -1,27 +1,18 @@
-let list1 = [a, b, c];
+let list1 = ['a', 'b', 'c'];
 let list2 = [1, 2, 3];
 
 function zipList(data1, data2){
-  let newdata = [];
-  let sum41 = data1.length + data2.length;
-
-  let itter = 0;
-  for(let sum14 of sum41){
-    if((sum14%2) === 0){
-        newdata+= data1[itter];
+    let newData = [];
+    for(let i = 0; i < data1.length; i++){
+        newData.push(data1[i], data2[i]);
     }
-    if((sum14%2) === 1){
-        newdata+= data2[itter];
-        itter++;
-    }
-  }
-  return newdata;
+    return newData;
 }
 
 console.log(zipList(list1, list2));
 
 function zipListTheSimpleWay(data1, data2){
-    return _.zip(list1, list2);
+    return _.flatten(_.zip(data1, data2));
 }
 
 console.log(zipListTheSimpleWay(list1, list2));
